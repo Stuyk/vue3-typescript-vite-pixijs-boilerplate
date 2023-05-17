f3d0f7ce3b4e53b0a0d8c83e2dc76764e252fb50
Mao Mr <49724027+hairyf@users.noreply.github.com>
2023-05-17 19:24:11 +0800

    refactor(renderer): use vue3-pixi renderer and update deps (#2)

---
+++ src/components/AlphaTransition.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { PixiTransition } from 'vue3-pixi'
+</script>
+
+<template>
+  <pixi-transition
+    :duration="300"
+    :before-enter="{ alpha: 0 }"
+    :enter="{ alpha: 1 }"
+    :leave="{ alpha: 0 }"
+  >
+    <slot />
+  </pixi-transition>
+</template>
